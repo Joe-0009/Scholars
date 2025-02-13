25ba973d496bc94d63427d6956a3f045dfa9fea5
Joe-0009 <youssefrachidi1994@gmail.com>
2025-02-13 14:25:09 +0100

    current time

--- realscholars/philosophers.c
+++ realscholars/philosophers.c
@@ -1,5 +1,13 @@
 #include "philosophers.h"
 
+long long get_current_time(void)
+{
+    struct timeval time;
+
+    gettimeofday(&time, NULL);
+    return ((time.tv_sec * 1000) + (time.tv_usec / 1000));
+}
+
 int init_program(t_program *program)
 {
     int i;
